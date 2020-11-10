506effe7c379e9dd54051fa78e6dbd81f7d55d38
fahri13 <noname13183597@gmail.com>
2020-11-10 14:04:19 +0700

    tambah-contact

---
+++ appstarter/app/Controllers/Contact.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+class Contact extends BaseController
+{
+
+  public function index()
+  {
+    $data = ['title' => 'Contact'];
+    return view('contact/index', $data);
+  }
+}
+
+
+?>
--- appstarter/app/Views/dashboard/index.php
+++ appstarter/app/Views/dashboard/index.php
@@ -13,7 +13,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.html" title="HOME"><i class="ion-paper-airplane"></i> euro <span>travel</span></a>
+      <a class="navbar-brand" href="<?= base_url('/dashboard/index') ?>" title="HOME"><i class="ion-paper-airplane"></i> euro <span>travel</span></a>
     </div> <!-- /.navbar-header -->
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -22,6 +22,26 @@
         <li class="active"><a href="<?= base_url('/dashboard/index') ?>">Home</a></li>
         <li><a href="<?= base_url('/about/index') ?>">about</a></li>
         <li><a href="<?= base_url('/service/index') ?>">services</a></li>
+
+        <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Account</a>
+
+
+      <?php if(logged_in()) : ?>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="<?= base_url('') ?>">Profil</a><br>
+          <a class="dropdown-item" href="<?= base_url('/logout') ?>">Log Out</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      <?php else : ?>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="<?= base_url('/login') ?>">SignIn</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        <?php endif; ?>
+
+
         <li><a href="<?= base_url('/contact/index') ?> ">contact</a></li>
       </ul> <!-- /.nav -->
       </div><!-- /.navbar-collapse -->
